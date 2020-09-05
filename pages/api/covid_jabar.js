6703d3717a891f74df2f5af07e6606e87f839f6a
zialdiansyah <ziaulhafizda@gmail.com>
2020-09-05 18:18:24 +0700

    add api

---
+++ pages/api/covid_jabar.js
@@ -0,0 +1,47 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async (req, res) => {
+  if(req.method !== 'GET') {
+    return res.status(405).end(JSON.stringify({message: 'Sorry'}))
+  }
+  const total_cases = await getTotalCases()
+  const today_cases = await getTodayCases()
+
+  res.status(200).end(JSON.stringify({total_cases, today_cases}))
+}
+
+function getToday () {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
+
+  today = yyyy+ '-' + mm + '-' + dd;
+  return today
+}
+
+async function getTodayCases () {
+  const response = await fetch('https://covid19-public.digitalservice.id/api/v1/rekapitulasi/jabar/harian')
+  const { data } = await response.json()
+  const today = getToday()
+  
+  const today_data = await data.content.filter((el, index, arr) => el.tanggal === today)
+  const today_case = {
+    positif: today_data[0].positif,
+    sembuh: today_data[0].sembuh,
+    meninggal: today_data[0].meninggal
+  }
+  console.log(today_case)
+  return today_case
+}
+
+async function getTotalCases () {
+  const response = await fetch('https://covid19-public.digitalservice.id/api/v1/rekapitulasi/jabar')
+  const { data } = await response.json()
+  const total_cases = {
+    positif: data.content.positif,
+    sembuh: data.content.sembuh,
+    meninggal: data.content.meninggal
+  }
+  return total_cases
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,82 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ data }) {
   return (
-    <div className={styles.container}>
+    <div className="container mx-auto">
       <Head>
-        <title>Create Next App</title>
+        <title>Covid19 Jabar</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <h1 className="text-4xl font-bold text-center mb-20">Covid19 Jabar</h1>
+      <div className="flex-col justify-center mb-20">
+        <div className="flex-1 rounded overflow-hidden shadow-lg bg-yellow-400 mx-2 mb-4">
+          <div className="px-6 py-4 text-center text-xl">
+            <div className="font-bold mb-2">Positif</div>
+            <div>{data.total_cases.positif || 0}</div>
+            {
+              data.today_cases.positif ?
+                <div className="inline-flex items-center text-sm">
+                  (
+                  <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" height="20" width="20" fill="currentColor">
+                    <path fillRule="evenodd" d="M4.293 15.707a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414 0zm0-6a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 5.414 5.707 9.707a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                  </svg>
+                  <span>{data.today_cases.positif}</span>
+                  )
+                </div>
+                :
+                ''
+            }
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className="flex-1 rounded overflow-hidden shadow-lg bg-red-500 mx-2 mb-4">
+          <div className="px-6 py-4 text-center text-xl">
+            <div className="font-bold mb-2">Wafat</div>
+            <div>{data.total_cases.meninggal || 0}</div>
+            {
+              data.today_cases.meninggal ?
+                <div className="inline-flex items-center  text-sm">
+                  (
+                  <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" height="20" width="20" fill="currentColor">
+                    <path d="M4.293 15.707a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414 0zm0-6a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 5.414 5.707 9.707a1 1 0 01-1.414 0z"/>
+                  </svg>
+                  <span>{data.today_cases.meninggal}</span>
+                  )
+                </div>
+                :
+                ''
+            }
+          </div>
+        </div>
+        <div className="flex-1 rounded overflow-hidden shadow-lg bg-green-500 mx-2">
+          <div className="px-6 py-4 text-center text-xl">
+            <div className="font-bold mb-2">Pulih</div>
+            <div>{data.total_cases.sembuh || 0}</div>
+            {
+              data.today_cases.sembuh ?
+                <div className="inline-flex items-center text-sm">
+                  (
+                  <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M4.293 15.707a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414 0zm0-6a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 5.414 5.707 9.707a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                  </svg>
+                  <span>{data.today_cases.sembuh}</span>
+                  )
+                </div>
+                :
+                ''
+            }
+          </div>
+        </div>
+      </div>
+      <div className="text-sm text-center">Using Pikobar Jabar Public API</div>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`http://localhost:3000/api/covid_jabar`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
