546b68ee4d9ae37147d30886a53f517abdaac8bc
zialdiansyah <ziaulhafizda@gmail.com>
2020-09-14 13:19:47 +0700

    update manifest

--- pages/index.js
+++ pages/index.js
@@ -24,13 +24,13 @@ export default function Home() {
         <meta name='application-name' content={APP_NAME} />
 
         <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='theme-color' content='#FFFFFF' />
+        <meta name='theme-color' content='#48BB78' />
         
         <meta name='apple-mobile-web-app-title' content={APP_NAME} />
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-        
         <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+        
         <link rel='manifest' href='/manifest.json' />
         <link rel='shortcut icon' href='/icons/favicon.ico' />
       </Head>
