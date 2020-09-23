24a9fe68359bd773da920a5a624f8fae69a2e6da
zialdiansyah <ziaulhafizda@gmail.com>
2020-09-23 16:17:33 +0700

    change icon image

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps }) {
         <meta name='apple-mobile-web-app-title' content={APP_NAME} />
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+        <link rel='apple-touch-icon' sizes='192x192' href='/icons/apple-touch-icon.png' />
         
         <link rel='manifest' href='/manifest.json' />
         <link rel='shortcut icon' href='/icons/favicon.ico' />
